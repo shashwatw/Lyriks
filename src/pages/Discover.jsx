46305a70ed09283dac26902f3646536c7ae75c60
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-01-10 11:16:40 +0530

    Starter Files

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,12 @@
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants';
+
+const Discover = () => {
+  const genreTitle = 'Pop';
+
+  return (
+    <div className="flex flex-col" />
+  );
+};
+
+export default Discover;
